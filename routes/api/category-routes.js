656df48fb5424e1215ac5b9a2a96bab6b0bfa46a
Lauren Goss <laurenlgoss98@gmail.com>
2021-09-08 11:08:44 -0400

    added GET api/categories/

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,11 +1,22 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
-
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    // Find all categories
+    const categoryData = await Category.findAll({
+      // Include associated products
+      include: [
+        {
+          model: Product,
+          attributes: ['product_name'],
+        },
+      ],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
